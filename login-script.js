7879b8500eea0f4b6dee5a7f353c1fb8e09b3596
Your Name <005961913@coyote.csusb.edu>
2021-03-14 21:08:45 -0700

    Cleaned up the css. general css now in style

--- login-script.js
+++ login-script.js
@@ -2,6 +2,7 @@
 // https://www.youtube.com/watch?v=4d-gIPGzmK4&list=PL4cUxeGkcC9itfjle0ji1xOZ2cjRGY_WB&ab_channel=TheNetNinja
 // https://www.youtube.com/watch?v=aN1LnNq4z54&list=PL4cUxeGkcC9jUPIes_B8vRjn1_GaplOPQ&ab_channel=TheNetNinja            
 
+<<<<<<< Updated upstream
 
 const loginBar;
 const signUp;
@@ -9,14 +10,36 @@ const logout;
 const login;
 const submitData;
 const username;    
+=======
+//login information
+const LoginEmail = document.querySelector(".loginEmail");
+const LoginPassword = document.querySelector(".loginPassword");
+const Login = document.querySelector("#loginmodal");
+
+//navbar information
+const UserDisplay = document.querySelector("#welcome");
+const NavLogin = document.querySelector("#login");
+const NavLogout = document.querySelector("#logout");
+
+//REEEEEEEEEEEEEEEEEEEEEEEEEEEEEE
+function hideLoginSignup() {
+    document.querySelector(".loginSignup").style.display = 'none';
+}
+>>>>>>> Stashed changes
 
 //listen for auth status changes, returns user if user is considered "logged in"
 auth.onAuthStateChanged(user => {
-    if(user)
-    {
+    if (user) {
         //user is logged in
+<<<<<<< Updated upstream
 
     }else{
+=======
+        console.log("There is a user logged in");
+        UserDisplay.textContent = "Welcome, " + user.email;
+        NavLogin.style.display = "none";
+    } else {
+>>>>>>> Stashed changes
         //user is logged out
 
     }
@@ -57,40 +80,45 @@ signUp.addEventListener("click", (e) =>
 
 
 //function will use firebase auth to log the user out
-function Log_Out()
-{
+function Log_Out() {
     auth.signOut();
 }
 
 //attaches logout function to logout button on page
+<<<<<<< Updated upstream
 logout.addEventListener("click", (e) =>
 {
     e.preventDefault();
+=======
+logout.addEventListener("click", (e) => {
+>>>>>>> Stashed changes
     Log_Out();
 });
 
 //function to log in user
-function Log_In()
-{
+function Log_In() {
     //can use squarebracket notation for a form object
     const email;
     const password;
 
     //if both forms have data inside of them
-    if((email != "")&&(password != ""))
-    {
+    if ((email != "") && (password != "")) {
         //basic sign-in method
-        auth.signInWithEmailAndPassword(email, password).then(cred =>
-        {
+        auth.signInWithEmailAndPassword(email, password).then(cred => {
             //clears information forms
         });
-    }else{
+    } else {
         //if one of the forms are empty
         alert("Please fill in all login information!");
     }
 }
+<<<<<<< Updated upstream
 login.addEventListener("click", (e) =>
 {
+=======
+
+Login.addEventListener("click", (e) => {
+>>>>>>> Stashed changes
     e.preventDefault();
     Log_In();
 });
---
+++ signup/sign-up-script.js
@@ -0,0 +1,96 @@
+// sign-up-script.js
+// https://www.youtube.com/watch?v=4d-gIPGzmK4&list=PL4cUxeGkcC9itfjle0ji1xOZ2cjRGY_WB&ab_channel=TheNetNinja
+// https://www.youtube.com/watch?v=aN1LnNq4z54&list=PL4cUxeGkcC9jUPIes_B8vRjn1_GaplOPQ&ab_channel=TheNetNinja            
+
+//sign-up informaiton
+const signUpEmailInput = document.querySelector("#signUpEmail");
+const signUpPasswordInput = document.querySelector("#signUpPassword");
+const signUpPasswordInput2 = document.querySelector("#signUpReenterPassword");
+const signUpButton = document.querySelector("#createAccount");
+
+//login information
+const LoginEmail = document.querySelector(".loginEmail");
+const LoginPassword = document.querySelector(".loginPassword");
+const SignUpLogin = document.querySelector("#signupinfoLogin");
+
+//navbar information
+const UserDisplay = document.querySelector("#welcome");
+const NavLogin = document.querySelector("#login");
+const NavLogout = document.querySelector("#logout");
+
+//listen for auth status changes, returns user if user is considered "logged in"
+auth.onAuthStateChanged(user => {
+    if (user) {
+        //user is logged in
+        console.log("There is a user logged in");
+        UserDisplay.textContent = "Welcome, " + user.email;
+    } else {
+        //user is logged out
+        console.log("There is no user logged in");
+        UserDisplay.textContent = "";
+    }
+});
+
+//function to sign up the user
+function Sign_Up() {
+    //can use squarebracket notation for a form object
+    const email = signUpEmailInput.value;
+    const password = signUpPasswordInput.value;
+    const passwordConfirm = signUpPasswordInput2.value;
+    if (password == passwordConfirm) {
+        //if both feilds have informatoin inside of them, and the password is of proper length:
+        if ((email != "") && (password != "") && (password.length >= 6)) {
+            //uses basic firebase auth sign-up method.
+            auth.createUserWithEmailAndPassword(email, password).then(cred => {
+                //clear input feilds
+                signUpEmailInput.value = "";
+                signUpPasswordInput.value = "";
+                signUpPasswordInput2.value = "";
+                window.location.href = "../index.html";
+                alert("Signed up Succesffuly");
+            });
+        } else if (password.length < 6) //if password is not of proper length
+        {
+            alert("Password must more than 6 characters!");
+        } else {
+            //if either form is not filled in
+            alert("Please fill in all login information!");
+        }
+    } else {
+        alert("Passwords do not match.");
+    }
+
+
+}
+//attaches our sign-up button to our sign up function
+//Using default event (E) allows us to use the preventDefault method, which doens't make the page refresh every time somthing happens
+signUpButton.addEventListener("click", (e) => {
+    e.preventDefault();
+    Sign_Up();
+});
+
+//function to log in user
+function Log_In() {
+    //can use squarebracket notation for a form object
+    const email = LoginEmail.value;
+    const password = LoginPassword.value;
+
+    //if both forms have data inside of them
+    if ((email != "") && (password != "")) {
+        //basic sign-in method
+        auth.signInWithEmailAndPassword(email, password).then(cred => {
+            //clears information forms
+            LoginEmail.value = "";
+            LoginPassword.value = "";
+            window.location.href = "../index.html";
+            alert("Logged In Successfully");
+        });
+    } else {
+        //if one of the forms are empty
+        alert("Please fill in all login information!");
+    }
+}
+SignUpLogin.addEventListener("click", (e) => {
+    e.preventDefault();
+    Log_In();
+});
